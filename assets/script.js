e80441f3bc7cc036c16f4acf98fb5ad09b4d6497
Amaryllis Pittin <amaryllispittin@gmail.com>
2023-12-01 13:25:50 +0100

    jour 2 - 6: premers pas sur javascripts, bullets points number

--- assets/script.js
+++ assets/script.js
@@ -17,4 +17,32 @@ const slides = [
 	}
 ]
 
+document.querySelector('.arrow_left').addEventListener('click', () => {
+	console.log("Vous avez cliqué hihi")
+})
 
+document.querySelector('.arrow_right').addEventListener('click', () => {
+	console.log("Vous avez cliqué haha")
+})
+
+/*****ul create element*******/
+let dotsContainer = document.querySelector('.dots');
+let ulElement = document.createElement('ul');
+
+let parentElement = document.querySelector('ul');
+let childElements = parentElement.querySelectorAll('li');
+let numberOfPoints = childElements.length;
+
+for (let i=0; i < numberOfPoints; i++) {
+	let liElement = document.createElement('li');
+	liElement.classList.add('dot');
+	ulElement.appendChild(liElement);
+}
+
+dotsContainer.appendChild(ulElement);
+ulElement.classList.add('dots');
+
+let firstLi = ulElement.querySelector('li:first-child');
+firstLi.classList.add('dot_selected');
+
+console.log("Nombre de petits chibidis:", numberOfPoints);
