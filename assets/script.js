6ff4ed6989fce00ac66f9d4853af570f31f60597
Amaryllis Pittin <amaryllispittin@gmail.com>
2023-12-13 09:47:31 +0100

    final: fin du travail sur les bullets points

--- assets/script.js
+++ assets/script.js
@@ -19,7 +19,9 @@ const slides = [
 
 /*****CLASS for SLIDES*****/
 
+const dotsContainer = document.querySelector('.dots');
 const imgBanner = document.querySelector(".banner__all-img");
+const numberOfPoints = slides.length;
 let currentIndex = 0;
 
 slides.forEach((slide, index) => {
@@ -36,7 +38,7 @@ slides.forEach((slide, index) => {
 updateCarouselContent();
 
 function updateCarouselContent() {
-	const activeIndex = 0;
+	let activeIndex = 0;
 	imgBanner.innerHTML = '';
 	const firstImg = document.createElement('img');
 	firstImg.src = slides[activeIndex].image;
@@ -48,17 +50,39 @@ function updateCarouselContent() {
 
 /*****bullet create element*******/
 
-let dotsContainer = document.querySelector('.dots');
-const dotElement = document.querySelectorAll('.dot');
+const dotElements = [];
 
-numberOfPoints = slides.length;
-
-for (let i=0; i < numberOfPoints; i++) {
-let bulletElement = document.createElement("a");
-bulletElement.href = '';
-bulletElement.classList.add("dot");
-dotsContainer.appendChild(bulletElement);
+function createDots() {
+	for (let i=0; i < numberOfPoints; i++) {
+		let bulletElement = document.createElement("a");
+		bulletElement.href = '#';
+		bulletElement.classList.add("dot");
+		dotsContainer.appendChild(bulletElement);
+		dotElements.push(bulletElement);
+	}
 }
+
+function updateDots() {
+	dotElements.forEach(dot =>  {
+		dot.classList.remove('dot_selected');
+	});
+	dotElements[currentIndex].classList.add('dot_selected');
+}
+
+function changeSlide(index) {
+	currentIndex = index;
+
+	updateDots();
+}
+
+createDots();
+
+const firstDot = document.querySelector(".dot:first-of-type");
+if (firstDot) {
+firstDot.classList.add("dot_selected");
+}
+
+
 
 /*****Click for carousel arrows***/
 const TotalSlides = slides.length;
@@ -71,7 +95,8 @@ arrowRight.addEventListener('click', () => {
 	currentIndex = (currentIndex + 1) % TotalSlides;
 	const currentSlide = slides[currentIndex];
 	const ImageTagLine = currentSlide.tagLine;
-
+	dotIndex = (dotIndex + 1) % numberOfPoints;
+	changeSlide(currentIndex);
 
 	bannerImg.innerHTML = `<img src=${currentSlide.image} alt="Slide ${currentIndex + 1}" class="banner-img"> <p>${ImageTagLine}</p>`;
 	if (startTagLine) {
@@ -80,12 +105,17 @@ arrowRight.addEventListener('click', () => {
 });
 
 const arrowLeft = document.querySelector('.arrow_left');
+let dotIndex = dotElements;
 
 arrowLeft.addEventListener('click', () => {
 	currentIndex = (currentIndex - 1 + TotalSlides) % TotalSlides;
 	const currentSlide = slides[currentIndex];
 	const ImageTagLine = currentSlide.tagLine;
+	dotIndex = (dotIndex - 1 + numberOfPoints) % numberOfPoints;
+	changeSlide(currentIndex);
 
 	bannerImg.innerHTML = `<img src=${currentSlide.image} alt="Slide ${currentIndex - 1}" class="banner-img"> <p>${ImageTagLine}</p>`;
 	startTagLine.remove();
 })
+
+
